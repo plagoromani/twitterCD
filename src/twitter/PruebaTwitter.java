0243c66d457cf6bb933d9b398926d833569fc47e
Miguel <Miguel@miguel-PC>
2016-04-11 09:14:49 +0200

    commit inicial

---
+++ src/twitter/PruebaTwitter.java
@@ -0,0 +1,17 @@
+package twitter;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+/**
+ *
+ * @author Miguel
+ */
+public class PruebaTwitter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Twitter miTwitter = new TwitterFactory().getInstance();
+    }
+    
+}
